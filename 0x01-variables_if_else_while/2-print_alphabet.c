544ed71e4052db7943e583018988495988bb1d6b
Akanimoh George <30466162+umjoji@users.noreply.github.com>
2022-09-09 14:48:12 +0100

    Add alphabet printer

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	n = 97;
+	while (n < 123)
+	{
+		putchar(n);
+		n++;
+	}
+	return (0);
+}
