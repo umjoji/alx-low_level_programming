d27564c9fcb7eaea6eb988bedfcf331a89c686e4
Akanimoh George <30466162+umjoji@users.noreply.github.com>
2022-09-09 15:31:08 +0100

    Add alphabt printer

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	int n;
+	int m;
+
+	n = 97;
+	do {
+
+		if (n == 101 || n == 113)
+		{
+			n = n + 1;
+			continue;
+		}
+		putchar(n);
+		n++;
+	} while (n < 123);
+	putchar(10);
+	return (0);
+}
