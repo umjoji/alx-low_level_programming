a784aff860ff627313c1ee2ef2b5c4a40d8d7fcf
Akanimoh George <30466162+umjoji@users.noreply.github.com>
2022-09-09 15:45:43 +0100

    Add number printer

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	int n;
+	int m;
+
+	n = 0;
+	do {
+		printf("%d", n);
+		n++;
+	} while (n < 10);
+	putchar(10);
+	return (0);
+}
