528e47556a29ab6fe076b40b395d27a92d118ed1
Akanimoh George <30466162+umjoji@users.noreply.github.com>
2022-09-13 15:05:06 +0100

    Add 0-putchar and README

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char s[9] = "_putchar";
+
+	printf("%s\n", s);
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+#Functions and Nested Loops
+C Programs on functions and nested loops
