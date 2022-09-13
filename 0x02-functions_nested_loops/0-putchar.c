a8364dc1858d97ac19216db574543b9f5396618d
Akanimoh George <30466162+umjoji@users.noreply.github.com>
2022-09-13 15:55:41 +0100

    Add 0-putchar corrected

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,23 @@
-#include "_putchar.c"
+#include "main.h"
 
 /**
  * main - Entry point
  *
  * Return: always 0
  */
+
+
 int main(void)
 {
-	char s[9] = "_putchar";
-	int i = 0;
-
-	while (i < 9)
-	{
-		_putchar(s[i]);
-		i++;
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
