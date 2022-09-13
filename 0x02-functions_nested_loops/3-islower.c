0cc8351e73f8e995bcb0a60a0f06278bcdea2356
Akanimoh George <30466162+umjoji@users.noreply.github.com>
2022-09-13 17:25:10 +0100

    Add updated main.h and 3-islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h" 
+
+/**
+ * _islower - checks if a character is upper or lower case
+ *
+ * @c: character to be checked
+ *
+ * Return: 1 for yes, 0 for no
+ */
+int _islower(int c)
+{
+	if (c > 96 && c < 123)
+	{
+		return (1);
+	}
+	else
+	{
+
+		return (0);
+	}
+}
