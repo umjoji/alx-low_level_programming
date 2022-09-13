d7d29b642c6e6cd131539ab3451959a493939366
Akanimoh George <30466162+umjoji@users.noreply.github.com>
2022-09-14 00:06:27 +0100

    Fix 11-print_to_98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,7 +12,7 @@ void print_to_98(int n)
 {
 	if (n == 98)
 	{
-		printf("%d", n);
+		printf("%d\n", n);
 	}
 	else
 	{
@@ -30,7 +30,7 @@ void print_to_98(int n)
 			}
 			else
 			{
-				printf("%d, ", n);
+				printf("%d\n", n);
 				break;
 			}
 		}
