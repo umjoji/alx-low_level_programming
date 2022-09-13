edb3dd8f605c63e9598ba542d81420ce2d3c6176
Akanimoh George <30466162+umjoji@users.noreply.github.com>
2022-09-13 18:14:18 +0100

    Add 6-abs and updated main.h

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,26 @@
+include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ *
+ * @i: integer to be checked
+ *
+ * Return: Absolute value of int
+ */
+
+int _abs(int i)
+{
+	if (i > 1)
+	{
+		return (i);
+	}
+	else if (i == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		i = -i;
+		return (i);
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,3 +2,4 @@ int _putchar(char c);
 int _islower(int c); 
 int _isalpha(int c);
 int print_sign(int n);
+int _abs(int i);
