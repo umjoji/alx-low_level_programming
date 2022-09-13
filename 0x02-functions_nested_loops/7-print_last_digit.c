8764fde4731296c699b8d9c797664b909153e34f
Akanimoh George <30466162+umjoji@users.noreply.github.com>
2022-09-13 18:32:42 +0100

    Add 7-print_last_digit and updated main.h

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Returns the value of the last digit of n
+ *
+ * @n: the number
+ *
+ * Return: The last digit of n
+ */
+
+int print_last_digit(int n)
+{
+	int m;
+
+	m = n % 10;
+	if (m >= 0)
+	{
+		return (m);
+	}
+	else
+	{
+		m = -m;
+		return (m);
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,3 +3,4 @@ int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int i);
+int print_last_digit(int n);
