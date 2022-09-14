9a37eb8ef729276319036ddc9c2f359809d73f65
Akanimoh George <30466162+umjoji@users.noreply.github.com>
2022-09-14 14:01:41 +0100

    Fix 100-times_table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,45 +12,44 @@ void print_times_table(int n)
 {
 	int i, j, num;
 
-	for (i = 0; i < (n + 1); i++)
+	while (num < 100)
 	{
-		for (j = 0; j < (n + 1); j++)
+		for (i = 0; i < (n + 1); i++)
 		{
-			num = i * j;
-			if (n >= 100)
+			for (j = 0; j < (n + 1); j++)
 			{
-				break;
+				num = i * j;
+				if (j == 0)
+				{
+					_putchar(num + '0');
+				}
+				else if (num >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((num / 100) + 48);
+					_putchar(((num % 100) / 10) + 48);
+					_putchar((num % 10) + 48);
+				}
+				else if (num < 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(num + '0');
+				}
+				else if (num >= 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((num / 10) + 48);
+					_putchar((num % 10) + 48);
+				}
 			}
-			if (j == 0)
-			{
-				_putchar(num + '0');
-			}
-			else if (num >= 100)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar((num / 100) + 48);
-				_putchar(((num % 100) / 10) + 48);
-				_putchar((num % 10) + 48);
-			}
-			else if (num < 10)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(num + '0');
-			}
-			else if (num >= 10)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar((num / 10) + 48);
-				_putchar((num % 10) + 48);
-			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 
 }
