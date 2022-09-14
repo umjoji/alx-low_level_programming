35ebd2b07bf50c7544b318989bc3f7cf18684e71
Akanimoh George <30466162+umjoji@users.noreply.github.com>
2022-09-14 03:21:07 +0100

    Add 8-jack_bauer and update main.h

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Print every minute of the day
+ *
+ * Return: Void
+ */
+
+void jack_bauer(void)
+{
+	int i, j;
+
+	for (i = 0; i < 24; i++)
+	{
+		for (j = 0; j < 60; j++)
+		{
+			_putchar((i / 10) + 48);
+			_putchar((i % 10) + 48);
+			_putchar(':');
+			_putchar((j / 10) + 48);
+			_putchar((j % 10) + 48);
+			_putchar('\n');
+		}
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,3 +9,4 @@ void print_to_98(int n);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 void times_table(void);
+void jack_bauer(void);
