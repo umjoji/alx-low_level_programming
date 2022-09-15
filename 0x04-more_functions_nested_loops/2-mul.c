b270c82739901639c7a109ef87577f9235f700a4
Akanimoh George <30466162+umjoji@users.noreply.github.com>
2022-09-15 17:04:10 +0100

    Add 2-mul, 3-print_numbers and update main.h

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: integer a
+ * @b: integer b
+ * Return: product of a and b
+ */
+
+int mul(int a, int b)
+{
+	int product;
+
+	product = a * b;
+	return (product);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main"
+
+/**
+ * print_numbers - print numbers from 0 to 9
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58, i++)
+	{
+		_putchar(i);
+	}
+	_putchar(\n);
+}
