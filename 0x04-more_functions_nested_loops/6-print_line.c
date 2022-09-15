ba573281abc8518c212092203782f2ad3959c02e
Akanimoh George <30466162+umjoji@users.noreply.github.com>
2022-09-15 18:17:48 +0100

    Update 6-print_line and main.h

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line
+ * @n: integer
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		if (n != 0)
+		{
+			_putchar(95);
+		}
+		else if (n == 0)
+		{
+			break;
+		}
+	}
+	_putchar('\n');
+}
