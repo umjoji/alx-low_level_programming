b5db7f123025dace93c6376170f49d719f0b9164
Akanimoh George <30466162+umjoji@users.noreply.github.com>
2022-09-15 02:22:19 +0100

    Add 101-natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *sum_of_multiples_3_and_5 - sumes the multiples of 3 and 5 below 1024
+ *@n: integer below 1024
+ *Return: sum of multiples of three and five
+ */
+int sum_of_multiples_3_and_5(int n)
+{
+	int i, sum;
+
+	sum = 0;
+	for (i = 1; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+		{
+			sum += i;
+		}
+	}
+	return (sum);
+}
+
