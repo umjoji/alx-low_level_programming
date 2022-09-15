089a93aad089fc4c3a83dec61a3571e2cb462244
Akanimoh George <30466162+umjoji@users.noreply.github.com>
2022-09-15 03:21:39 +0100

    Add 103-fibonacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <inttypes.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	int64_t num1 = 1;
+	int64_t num2 = 2;
+	int64_t nextnum = num1 + num2;
+	int64_t sum = 0;
+
+	for (i = 4; i <= 50; i++)
+	{
+		num1 = num2;
+		num2 = nextnum;
+		nextnum = num1 + num2;
+
+		if (nextnum >= 4000001)
+		{
+			break;
+		}
+		else if (nextnum % 2 == 0)
+		{
+			sum += nextnum;
+		}
+	}
+	printf("%" PRId64 "\n", sum);
+	return (0);
+}
