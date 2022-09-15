a3fdc38707debe946e6366438787864a61a73808
Akanimoh George <30466162+umjoji@users.noreply.github.com>
2022-09-15 10:01:21 +0100

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -12,29 +12,29 @@ int main(void)
 {
 	/* Divide the number into two parts and create holders for them */
 	int i;
-	uint64_t first_part_num1 = 0, second_part_num1 = 1;  
-	uint64_t first_part_num2 = 0, second_part_num2 = 2;
-	uint64_t number_holder1, number_holder2, number_holder3;
+	uint64_t fpart_num1 = 0, spart_num1 = 1;  
+	uint64_t fpart_num2 = 0, spart_num2 = 2;
+	uint64_t num_holder1, num_holder2, num_holder3;
 
-	printf("%" PRId64 ", ", second_part_num1);
-	printf("%" PRId64 ", ", second_part_num2);
+	printf("%" PRId64 ", ", spart_num1);
+	printf("%" PRId64 ", ", spart_num2);
 
 	for (i = 3; i <= 98; i++)
 	{
-		if (second_part_num1 + second_part_num2 > LARGEST || first_part_num1 > 0 || first_part_num2 > 0)
+		if (spart_num1 + spart_num2 > LARGEST || fpart_num1 > 0 || fpart_num2 > 0)
 		{
-			number_holder1 = (second_part_num1 + second_part_num2) / LARGEST;
-			number_holder2 = (second_part_num1 + second_part_num2) % LARGEST;
-			number_holder3 = first_part_num1 + first_part_num2 + number_holder1;
-			first_part_num1 = first_part_num2, first_part_num2 = number_holder3;
-			second_part_num1 = second_part_num2, second_part_num2 = number_holder2;
-			printf("%lu%010lu", first_part_num2, second_part_num2);
+			num_holder1 = (spart_num1 + spart_num2) / LARGEST;
+			num_holder2 = (spart_num1 + spart_num2) % LARGEST;
+			num_holder3 = fpart_num1 + fpart_num2 + num_holder1;
+			fpart_num1 = fpart_num2, fpart_num2 = num_holder3;
+			spart_num1 = spart_num2, spart_num2 = num_holder2;
+			printf("%lu%010lu", fpart_num2, spart_num2);
 		}
 		else
 		{
-			number_holder2 = (second_part_num1 + second_part_num2);
-			second_part_num1 = second_part_num2, second_part_num2 = number_holder2;
-			printf("%" PRId64 "", second_part_num2);
+			num_holder2 = (spart_num1 + spart_num2);
+			spart_num1 = spart_num2, spart_num2 = num_holder2;
+			printf("%" PRId64 "", spart_num2);
 		}
 		if (i != 98)
 		{
