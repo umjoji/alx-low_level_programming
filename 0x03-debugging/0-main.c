b962bca786f9c94e299f3f33d17d3535525cb08f
Akanimoh George <30466162+umjoji@users.noreply.github.com>
2022-09-15 11:01:50 +0100

    Fix 0-main

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -7,10 +7,14 @@
 
 int main(void)
 {
-        int i;
+        int i, r;
 
         i = 0;
-        positive_or_negative(i);
+        r = positive_or_negative(i);
+	if (r = 0)
+	{
+		printf("%d is zero", r);
+	}
 
         return (0);
 }
