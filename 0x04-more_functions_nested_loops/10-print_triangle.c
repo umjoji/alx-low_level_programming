65fb85269139e9b75ee19ee0abbff05b749247a3
Akanimoh George <30466162+umjoji@users.noreply.github.com>
2022-09-16 16:22:28 +0100

    Update 10-triangles main.h

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_triangle - prits a square
+ * @size: size of sqaure
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				if (j >= size - 1 - i || i == size - 1)
+				{
+					_putchar('#');
+				}
+				else
+				{
+					_putchar(' ');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -8,3 +8,5 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void more_numbers(void);
+void print_triangle(int size);
+
