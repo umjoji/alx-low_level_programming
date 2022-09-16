c8f12a2830c05c4b5b9fc3e053b1c2ec2318fabc
Akanimoh George <30466162+umjoji@users.noreply.github.com>
2022-09-16 12:56:10 +0100

    Add fizzbuzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,19 +13,23 @@ int main(void)
 	{
 		if (i % 15 == 0)
 		{
-			printf("Fizz Buzz ");
+			printf(" Fizz Buzz");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz ");
+			printf(" Buzz");
 		}
 		else if (i % 3 == 0)
 		{
-			printf("Fizz ");
+			printf(" Fizz");
+		}
+		else if (i == 1)
+		{
+			printf("%d", i);
 		}
 		else
 		{
-			printf("%d ", i);
+			printf(" %d", i);
 		}
 	}
 	putchar('\n');
