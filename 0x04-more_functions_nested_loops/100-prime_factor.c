c127ac93cf745d99f591e6e3d55b3b53d4c1a2df
Akanimoh George <30466162+umjoji@users.noreply.github.com>
2022-09-17 09:12:06 +0100

    Add 100-prime factor solution

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry poirt
+ * Return: 0
+ */
+
+int main(void)
+{
+	long int n = 612852475143;
+	long int i = 3;
+
+	for (; i < 12057; i +=2)
+	{
+		while (n % i == 0 && n != i)
+		{
+			n /= i;
+		}
+	}
+	printf("%li\n", n);
+	return (0);
+}
