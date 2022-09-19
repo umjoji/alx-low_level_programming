119898d1aefd519d9e4e8359d34b763dfd862aa4
Akanimoh George <30466162+umjoji@users.noreply.github.com>
2022-09-20 00:36:23 +0100

    Add 7-puts_half and update main.h

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * puts_half - prints half of a string
+ * @str: string
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int i = 0;
+	int len = _strlen(str);
+
+	for (i = len / 2;; i++)
+	{
+		if (str[i] == '\0')
+		{
+			break;
+		}
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,3 +6,4 @@ void print_rev(char *s);
 void swap_int(int *a, int *b);
 void rev_string(char *s);
 void puts2(char *str);
+void puts_half(char *str);
