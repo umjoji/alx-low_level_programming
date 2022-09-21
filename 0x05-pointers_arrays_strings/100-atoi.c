e7cfde707b3358553d96f0f9f56e3e2b23c052d6
Akanimoh George <30466162+umjoji@users.noreply.github.com>
2022-09-21 05:26:12 +0100

    Add 100-atoi, update main.h, delete 8-print_array

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * _atoi - converts a string to integer
+ * @s: string
+ * Return: integer in string
+ */
+
+int _atoi(char *s)
+{
+	unsigned int len = 0, size = 0, oi = 0, num_in_string = 1, m = 1, i;
+
+	while (s[len] != '\0')
+	{
+		if (size > 0 && s[len] < '0' || s[len] > '9')
+		{
+			break;
+		}
+
+		if (s[len] == '-')
+		{
+			num_in_string *= -1;
+		}
+
+		if (s[len] >= '0' && s[len] <= '9')
+		{
+			if (size > 0)
+			{
+				m *= 10;
+			}
+			size++;
+		}
+		len++;
+	}
+
+	for (i = count - size; i < count; i++)
+	{
+		oi = oi + ((*(s + i) - 48) * m);
+		m /=10;
+	}
+	
+	return (oi * num_in_string);
+}
