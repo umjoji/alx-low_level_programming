8bc0ba83d2028a9ece7b6fca43c537fee92dcea7
Akanimoh George <30466162+umjoji@users.noreply.github.com>
2022-09-21 05:43:44 +0100

    Add 101-keygen

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - generates keygen password
+ * Return: 0
+ */
+
+int main(void)
+{
+	int random_num = 0, check = 0;
+	time_t t;
+
+	srand((unsigned int) time(&t));
+	while (check < 2772)
+	{
+		random_num = rand() % 128;
+		if ((c + r) > 2772)
+		{
+			break;
+		}
+		check = check + random_num;
+		printf("%c", random_num);
+	}
+	
+	printf("%c\n", (2772 - check));
+
+	return (0);
