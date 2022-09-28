b685e1f7141cc8bdf83d04a826fd3edfb59f74f6
Akanimoh George <30466162+umjoji@users.noreply.github.com>
2022-09-28 03:55:32 +0100

    Fix 5-strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -5,7 +5,8 @@
  * _strstr - locates a substring
  * @haystack: the string to be searched
  * @needle: the string we are sarching for
- * Return: a pointer to the beginning of the located substring or null if not found
+ * Return: a pointer to the beginning of the located substring or
+ * null if not found
  */
 
 char *_strstr(char *haystack, char *needle)
@@ -13,13 +14,14 @@ char *_strstr(char *haystack, char *needle)
 	int num;
 
 	/* iterate through haystack */
-	for (num = 0; haystack[num] != '\0'; num++)
+	for (num = 0; *(haystack + num) != '\0'; num++)
 	{
 		if (_strcmp(haystack + num, needle) == 0)
 		{
 			return (haystack + num);
 		}
+		
 	}
-	
+
 	return ('\0');
 }
