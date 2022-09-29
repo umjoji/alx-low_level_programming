3774bf6b63d5e2c677abe7c3a7c35eb8ea09c932
Akanimoh George <30466162+umjoji@users.noreply.github.com>
2022-09-29 14:16:07 +0100

    add 4-pow_recursion and update mian.h

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - returns the value of x to the power of y
+ * @x: number
+ * @y: power
+ * Return: x to the power of y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
+
