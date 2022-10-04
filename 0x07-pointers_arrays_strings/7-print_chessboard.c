777caa597d716bd2c5735c0e9cbc38659a689be6
Akanimoh George <30466162+umjoji@users.noreply.github.com>
2022-10-04 10:28:10 +0100

    Add 7-print_chessboard and update main.h

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints an empty chessboard
+ * @a: input array pointer
+ * Return: void
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int num, num1;
+
+	for (num = 0; num < 64; num++)
+	{
+		/* print a new line when it gets to multiples of 8 */
+		if (num % 8 == 0 && num != 0)
+		{
+			num1 = num;
+			_putchar('\n');
+		}
+
+		_putchar(a[num / 8][num - num1]);
+	}
+	
+	_putchar('\n');
+}
