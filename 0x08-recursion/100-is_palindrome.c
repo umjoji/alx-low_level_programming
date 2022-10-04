7a654324c0b23c452d0e48cab38ff99a07801f37
Akanimoh George <30466162+umjoji@users.noreply.github.com>
2022-10-04 13:02:32 +0100

    Add 100-is_palindrome and update main.h

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * comparer - compares each character of the string
+ * @s: string
+ * @n1: smallest iterator
+ * @n2: biggest iterator
+ * Return: integer
+ */
+
+int comparer(char *s, int n1, int n2)
+{
+	if (*(s + n1) == *(s + n2))
+	{
+		if (n1 == n2 || n1 == n2 + 1)
+			return (1);
+		return (0 + comparer(s, n1 + 1, n2 -1));
+	}
+	return (0);
+}
+
+/**
+ * is_palindrome - checks for a palindrome
+ * @s: string
+ * Return: 1 if s is a palindrome else 0
+ */
+
+int is_palindrome(char *s)
+{
+	if (*s == '\0')
+	{
+		return (1);
+	}
+	return (comparator(s, 0, _strlen_recursion(s) - 1));
+}
