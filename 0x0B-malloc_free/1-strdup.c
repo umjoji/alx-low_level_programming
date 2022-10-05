9b9d6c4bedd932c893e191e34eb04f0cdfc64a68
Akanimoh George <30466162+umjoji@users.noreply.github.com>
2022-10-05 03:38:19 +0100

    Add 1-strdup, 9-strcpy, 2-strlen and update main.h

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,7 +11,7 @@ char *_strdup(char *str)
 {
 	char *dup_str;
 	unsigned int num1, num2;
-	
+
 	if (str == NULL)
 	{
 		return (NULL);
@@ -23,13 +23,13 @@ char *_strdup(char *str)
 
 	/* allocate the sapce for the duplicate string */
 	dup_str = (char *) malloc(sizeof(char) * (num1 + 1));
-	
+
 	if (dup_str == NULL)
 		return (NULL);
 
 	/* duplicate string */
 	for (num2 = 0; num2 <= num1; num2++)
 		dup_str[num2] = str[num2];
-	
+
 	return (dup_str);
 }
