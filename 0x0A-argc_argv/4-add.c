9dfa77c4a05419c1b602019d63e3b1cedfda4a97
Akanimoh George <30466162+umjoji@users.noreply.github.com>
2022-10-09 08:54:46 +0100

    add 4-add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -14,7 +14,7 @@ int main(int argc, char *argv[])
 	int num1, num2, add = 0;
 
 	/* for number of arguments */
-	for (num1 = 1; i < argc; num++)
+	for (num1 = 1; num1 < argc; num++)
 	{
 		for (num2 = 0; argv[num1][num2] != '\0'; num2++)
 		{
