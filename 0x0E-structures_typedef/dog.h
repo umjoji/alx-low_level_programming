118afa04ef7f5a38f47bfea156c6f858f64f0d12
Akanimoh George <30466162+umjoji@users.noreply.github.com>
2022-10-11 16:50:19 +0100

    Fix dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,17 +2,11 @@
 #define _DOG_H_
 
 /**
- * dog_t - typedef for struct dog
- */
-
-typedef struct dog dog_t;
-
-/**
  * struct dog - describes a dog
  * @name: name of dog
  * @age: age of dog
  * @owner: owner of dog
- * 
+ *
  * Description: this struct descibes a dog
  */
 
