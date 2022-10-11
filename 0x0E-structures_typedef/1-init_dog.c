298b186add3c244ceca469e8198c1d94d58df80a
Akanimoh George <30466162+umjoji@users.noreply.github.com>
2022-10-11 17:12:09 +0100

    Add 1-init_dog and update dog.h

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+
+/**
+ * init_dog - variable of type struct dog
+ * @d: dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Return: void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
