2f2e26aef1f26bf36176510e4fe4f08b80d434cf
Akanimoh George <30466162+umjoji@users.noreply.github.com>
2022-10-11 17:25:55 +0100

    Add 2-print_dog and update dog.h

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,29 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - prints a struct dog
+ * @d: dog
+ * Return: void
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		if (!(d->name))
+			printf("Name: (nil)\n");
+		else
+			printf("Name: %s\n", d->name);
+
+		if (!(d->age))
+			printf("Age: (nil)\n");
+		else
+			printf("Age: %.1f\n", d->age);
+
+		if (!(d->owner))
+			printf("Owner: (nil)\n");
+		else
+			printf("Owner: %s\n", d->owner);
+	}
+}
