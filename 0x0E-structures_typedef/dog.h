f57815dff3058f763b0c7926e089d1eb3af22310
Akanimoh George <30466162+umjoji@users.noreply.github.com>
2022-10-11 16:41:06 +0100

    Add main.h, README, and dog,h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+#endif
+
+/**
+ * struct dog - describes a dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ * 
+ * Description: this struct descibes a dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
