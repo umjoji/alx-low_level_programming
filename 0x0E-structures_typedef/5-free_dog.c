da26b95c1faec0ae48ba283fed08366ee592f7cd
Akanimoh George <30466162+umjoji@users.noreply.github.com>
2022-10-12 12:30:29 +0100

    Add 5-free_dog and update dog.h

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/** free_dog - frees dogs
+ * @d: stuct dog
+ * Return: void
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
