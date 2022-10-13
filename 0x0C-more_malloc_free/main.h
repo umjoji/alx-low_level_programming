acdeb94fa44bdfa52801dbeb7006f3bff8b1f604
Akanimoh George <30466162+umjoji@users.noreply.github.com>
2022-10-13 03:02:34 +0100

    add task files

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,9 +2,9 @@
 #define _MAIN_H_
 
 void *malloc_checked(unsigned int b);
-char *string_nconcat(char *s1, char *s2, unsigned in n);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
-void *realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
 #endif
