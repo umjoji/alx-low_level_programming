8eee538c8c5d7a21e1adab49bce3dd9795771329
Akanimoh George <30466162+umjoji@users.noreply.github.com>
2022-10-16 01:13:28 +0100

    Fix 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,4 +1,4 @@
-#include "3-calc-h"
+#include "3-calc.h"
 
 /**
  * main - check the code
@@ -13,12 +13,26 @@ int main(int argc, char *argv[])
 	int a, b;
 	int (*operation)(int, int);
 
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
 	if (argv[2][1])
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
+	operation = get_op_func(argv[2]);
+
+	if (operation == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
 
