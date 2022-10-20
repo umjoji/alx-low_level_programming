ed8e60a1b7cfa831b1377edcc45d655596439ddd
Akanimoh George <30466162+umjoji@users.noreply.github.com>
2022-10-20 17:55:53 +0100

    Add list_len and header

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements in a linked list
+ * @h - node/struct
+ *
+ * Return: number of elements
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t nodes = 0;
+
+	while (h) /* is not null */
+	{
+		h = h->next;
+		nodes++;
+	}
+	
+	return (nodes);
+}
+
