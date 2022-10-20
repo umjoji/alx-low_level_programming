b7634b6a54d463a1ce007783be79c2f2a4461a9c
Akanimoh George <30466162+umjoji@users.noreply.github.com>
2022-10-20 17:34:40 +0100

    Fix 0-print_list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -16,13 +16,10 @@ size_t print_list(const list_t *h)
 	{
 		/* if string in struct is null */
 		if (h->str == NULL)
-		{
 			printf("[0] (nil)\n");
-		}
 		else
-		{
 			printf("[%d] (%s)\n", h->len, h->str);
-		}
+		
 
 		h = h->next;	/* move to next node */
 		nodes++;
