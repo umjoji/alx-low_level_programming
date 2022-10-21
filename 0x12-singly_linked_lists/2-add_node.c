b3b8b4a14bdfde72bd91dd5bbec3239e04194b9a
Akanimoh George <30466162+umjoji@users.noreply.github.com>
2022-10-21 04:42:43 +0100

    Add 2-add_node and header file

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * add_node - adds a new node at the beginning of a list
+ * @head: head of the node
+ * @str: string to duplicated
+ *
+ * Return: new element address if success
+ * NULL if failed
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node = malloc(sizeof(list_t));
+
+	new_node->str = strdup(str);
+	new_node->len = strlen(str);
+	new_node->next = *head;
+	*head = new_node;
+
+	if (new_node->str != str)
+	{
+		return (NULL);
+	}
+	
+	return (*head);
+}
