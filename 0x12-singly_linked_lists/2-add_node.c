644687a7255a756762c27b98db8d2b7e1555ed9a
Akanimoh George <30466162+umjoji@users.noreply.github.com>
2022-10-21 04:54:27 +0100

    Add 2-add_node and header file

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -15,6 +15,12 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node = malloc(sizeof(list_t));
 
+	if (new == NULL)
+		return (NULL);
+
+	if (str == NULL)
+		return (NULL);::
+
 	new_node->str = strdup(str);
 	new_node->len = strlen(str);
 	new_node->next = *head;
