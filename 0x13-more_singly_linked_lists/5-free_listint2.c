e2eb8f3b921acefba0216ff5b1d19c46b4f91b98
Akanimoh George <30466162+umjoji@users.noreply.github.com>
2022-10-26 10:25:22 +0100

    Add 5-free_listint2 and update header file

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t list
+ * @head: head node
+ *
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *node = *head;
+	listint_t *next = NULL;
+
+	for (; node != NULL; node = next)
+	{
+		next = node->next;
+		free(node);
+	}
+
+	*head = NULL;
+}
